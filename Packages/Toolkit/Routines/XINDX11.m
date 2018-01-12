85aa02406ec0e23d1a13a7fc9e869ff2ecb320da
Christopher Edwards <christopher.edwards@krminc.com>
2018-01-12 07:38:58 -0500

    Revert "Remove block comments in each tag that describe each field to be XINDEXed"
    This reverts commit c3acd598175a7d84ba4b0289376beed3dc838f67.

--- Packages/Toolkit/Routines/XINDX11.m
+++ Packages/Toolkit/Routines/XINDX11.m
@@ -114,6 +114,19 @@ ADD ;Put code in UTILITY for processing
  ; ^UTILITY($J,1,INDRN,0,INDLC+1,0)={MUMPS Code (INDX)}
  ;
 SORT ;Process Sort Templates
+ ; ========  =====  ===============================  ===================================  =============================================
+ ; File      Field  Field Name                       Global Location                      Comments
+ ; ========  =====  ===============================  ===================================  =============================================
+ ; .401      1815   ROUTINE INVOKED                  ^DIBT(D0,ROU)
+ ; .401      1816   PREVIOUS ROUTINE INVOKED         ^DIBT(D0,ROUOLD)
+ ; .4014     10     GET CODE                         ^DIBT(D0,2,D1,GET)                   Part of Sort Field Data Subfile
+ ; .4014     11     QUERY CONDITION                  ^DIBT(D0,2,D1,QCON)                  Part of Sort Field Data Subfile
+ ; .4014     16     COMPUTED FIELD CODE              ^DIBT(D0,2,D1,CM)                    Part of Sort Field Data Subfile
+ ; .4014     20     SUBHEADER OUTPUT                 ^DIBT(D0,2,D1,OUT)                   Part of Sort Field Data Subfile
+ ; .401418   5      RELATIONAL CODE                  ^DIBT(D0,2,D1,2,D2,RCOD)             Part of Relational Jump Field Data Subfile
+ ; .401419   2      OVERFLOW CODE                    ^DIBT(D0,2,D1,3,D2,OVF0)             Part of Overflow Data Subfile
+ ; .4011624  4      DISPAR(0,n,OUT)                  ^DIBT(D0,BY0D,D1,2)                  Part of Sort Range Data For BY(0)
+ ; ========  =====  ===============================  ===================================  =============================================
  W !,"Processing Sort Templates",!
  S INDX=$S($L($P($G(^DIBT(B,"ROU")),U,1)):"D ^"_$P($G(^DIBT(B,"ROU")),U,1),1:";")
  S INDC=B_" ; "_INDL_" - ROUTINE INVOKED (#1815)"
@@ -160,6 +173,19 @@ SORT ;Process Sort Templates
  . D ADD
  Q
 FORM ;Process Forms
+ ; ========  =====  ===============================  ===================================  =============================================
+ ; File      Field  Field Name                       Global Location                      Comments
+ ; ========  =====  ===============================  ===================================  =============================================
+ ; .403      11     PRE ACTION                       ^DIST(.403,D0,11)
+ ; .403      12     POST ACTION                      ^DIST(.403,D0,12)
+ ; .403      14     POST SAVE                        ^DIST(.403,D0,14)
+ ; .403      20     DATA VALIDATION                  ^DIST(.403,D0,20)
+ ; .4031     11     PRE ACTION                       ^DIST(.403,D0,40,D1,11)
+ ; .4031     12     POST ACTION                      ^DIST(.403,D0,40,D1,12)
+ ; .4032     11     PRE ACTION                       ^DIST(.403,D0,40,D1,40,D2,11)
+ ; .4032     12     POST ACTION                      ^DIST(.403,D0,40,D1,40,D2,12)
+ ; .4032     98     COMPUTED MULTIPLE                ^DIST(.403,D0,40,D1,40,D2,COMP MUL)
+ ; ========  =====  ===============================  ===================================  =============================================
  W !,"Processing Forms",!
  S INDX=$S($L($P($G(^DIST(.403,B,11)),U,1)):$P($G(^DIST(.403,B,11)),U,1),1:";")
  S INDC=B_" ; "_INDL_" - PRE ACTION (#11)"
@@ -202,12 +228,23 @@ FORM ;Process Forms
  . . D ADD
  Q
 DIALOG ;Process Dialogs
+ ; ========  =====  ===============================  ===================================  =============================================
+ ; File      Field  Field Name                       Global Location                      Comments
+ ; ========  =====  ===============================  ===================================  =============================================
+ ; .84       6      POST MESSAGE ACTION              ^DI(.84,D0,6)
+ ; ========  =====  ===============================  ===================================  =============================================
  W !,"Processing Dialogs",!
  S INDX=$S($L($P($G(^DI(.84,B,6)),U,1)):$P($G(^DI(.84,B,6)),U,1),1:";")
  S INDC=B_" ; "_INDL_" - POST MESSAGE ACTION (#6)"
  D ADD
  Q
 HELP ;Process Help Frames
+ ; ========  =====  ===============================  ===================================  =============================================
+ ; File      Field  Field Name                       Global Location                      Comments
+ ; ========  =====  ===============================  ===================================  =============================================
+ ; 9.2       10.1   ENTRY EXECUTE STATEMENT          ^DIC(9.2,D0,10.1)
+ ; 9.2       10.2   EXIT EXECUTE STATEMENT           ^DIC(9.2,D0,10.2)
+ ; ========  =====  ===============================  ===================================  =============================================
  W !,"Processing Help Frames",!
  S INDX=$S($L($P($G(^DIC(9.2,B,10.1)),U,1)):$P($G(^DIC(9.2,B,10.1)),U,1),1:";")
  S INDC=B_" ; "_INDL_" - ENTRY EXECUTE STATEMENT (#10.1)"
@@ -218,12 +255,27 @@ HELP ;Process Help Frames
  D ADD
  Q
 KEY ;Process Security Keys
+ ; ========  =====  ===============================  ===================================  =============================================
+ ; File      Field  Field Name                       Global Location                      Comments
+ ; ========  =====  ===============================  ===================================  =============================================
+ ; 19.1      4      GRANTING CONDITION               ^DIC(19.1,D0,4)
+ ; ========  =====  ===============================  ===================================  =============================================
  W !,"Processing Security Keys",!
  S INDX=$S($L($P($G(^DIC(19.1,B,4)),U,1)):$P($G(^DIC(19.1,B,4)),U,1),1:";")
  S INDC=B_" ; "_INDL_" - GRANTING CONDITION (#4)"
  D ADD
  Q
 LIST ;Process List Templates
+ ; ========  =====  ===============================  ===================================  =============================================
+ ; File      Field  Field Name                       Global Location                      Comments
+ ; ========  =====  ===============================  ===================================  =============================================
+ ; 409.61    100    HEADER CODE                      ^SD(409.61,D0,HDR)
+ ; 409.61    102    EXPAND CODE                      ^SD(409.61,D0,EXP)
+ ; 409.61    103    HELP CODE                        ^SD(409.61,D0,HLP)
+ ; 409.61    105    EXIT CODE                        ^SD(409.61,D0,FNL)
+ ; 409.61    106    ENTRY CODE                       ^SD(409.61,D0,INIT)
+ ; 409.61    107    ARRAY NAME                       ^SD(409.61,D0,ARRAY)                 Holds a variable name prefaced by a space
+ ; ========  =====  ===============================  ===================================  =============================================
  W !,"Processing List Templates",!
  S INDX=$S($L($G(^SD(409.61,B,"HDR"))):$G(^SD(409.61,B,"HDR")),1:";")
  S INDC=B_" ; "_INDL_" - HEADER CODE (#100)"
@@ -250,6 +302,23 @@ LIST ;Process List Templates
  D ADD
  Q
 PROTOCOL ;Process Protocols
+ ; ========  =====  ===============================  ===================================  =============================================
+ ; File      Field  Field Name                       Global Location                      Comments
+ ; ========  =====  ===============================  ===================================  =============================================
+ ; 101       15     EXIT ACTION                      ^ORD(101,D0,15) E1,245
+ ; 101       20     ENTRY ACTION                     ^ORD(101,D0,20) E1,245
+ ; 101       24     SCREEN                           ^ORD(101,D0,24) E1,245
+ ; 101       26     HEADER                           ^ORD(101,D0,26) E1,245
+ ; 101       27     MENU HELP                        ^ORD(101,D0,27) E1,245
+ ; 101       100    ORDER PRINT ACTION               ^ORD(101,D0,100) E1,245
+ ; 101       100.1  ORDER CANCEL ACTION              ^ORD(101,D0,100.1) E1,245
+ ; 101       100.2  ORDER PURGE ACTION               ^ORD(101,D0,100.2) E1,245
+ ; 101       771    PROCESSING ROUTINE               ^ORD(101,D0,771) E1,245
+ ; 101       772    RESPONSE PROCESSING ROUTINE      ^ORD(101,D0,772) E1,245
+ ; 101       774    ROUTING LOGIC                    ^ORD(101,D0,774) E1,245
+ ; 101       21     REQUIRED VARIABLES               ^ORD(101,D0,21,D1,0)                 Required Variables sub file
+ ; 101.05    .02    METHOD ACTION                    ^ORD(101,D0,101.05,D1,1) E1,245      Method sub file
+ ; ========  =====  ===============================  ===================================  =============================================
  W !,"Processing Protocols",!
  S INDX=$S($L($E($G(^ORD(101,B,15)),1,245)):$E($G(^ORD(101,B,15)),1,245),1:";")
  S INDC=B_" ; "_INDL_" - EXIT ACTION (#15)"
@@ -312,6 +381,11 @@ PROTOCOL ;Process Protocols
  ;
  Q
 HL7AP ; Process HL7 Application Parameters
+ ; ========  =====  ===============================  ===================================  =============================================
+ ; File      Field  Field Name                       Global Location                      Comments
+ ; ========  =====  ===============================  ===================================  =============================================
+ ; 771.06    1      PROCESSING ROUTINE               ^HL(771,D0,MSG,D1,R)
+ ; ========  =====  ===============================  ===================================  =============================================
  W !,"Processing HL7 Application Parameters",!
  N SUB
  S SUB=""
@@ -322,6 +396,12 @@ HL7AP ; Process HL7 Application Parameters
  . D ADD
  Q
 RPC ; Process Remote Procedures
+ ; ========  =====  ===============================  ===================================  =============================================
+ ; File      Field  Field Name                       Global Location                      Comments
+ ; ========  =====  ===============================  ===================================  =============================================
+ ; 8994      .02    TAG                              ^XWB(8994,D0,0) Piece 2              Needs to be concatenated with ROUTINE
+ ; 8994      .03    ROUTINE                          ^XWB(8994,D0,0) Piece 3
+ ; ========  =====  ===============================  ===================================  =============================================
  W !,"Processing Remote Procedures",!
  S INDX=$S($L($P($G(^XWB(8994,B,0)),U,2))&($L($P($G(^XWB(8994,B,0)),U,3))):"D "_$P($G(^XWB(8994,B,0)),U,2)_"^"_$P($G(^XWB(8994,B,0)),U,3),1:";")
  S INDC=B_" ; "_INDL_" - TAG ROUTINE (#.02 & .03)"
